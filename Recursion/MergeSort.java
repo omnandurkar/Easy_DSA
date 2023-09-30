7cfdbebbc49a3aef63b0d52b31b410d5c394879f
Om Nandurkar <125724085+omnandurkar@users.noreply.github.com>
2023-10-01 00:22:11 +0530

    Update MergeSort.java
    updated Merge sort by Om

--- Recursion/MergeSort.java
+++ Recursion/MergeSort.java
@@ -64,3 +64,30 @@ public class MergeSort {
  *   Output:
  *   2 3 5 6 8 9
  */
+
+/* The provided Java code implements the Merge Sort algorithm, a widely used sorting algorithm based on the "Divide and Conquer" approach. Here's a short description of the code:
+
+1. `MergeSort` Class:
+   - Contains the main Merge Sort algorithm implementation.
+
+2. `mergeSort` Method:
+   - Takes an integer array (`arr`), start index (`si`), and end index (`ei`) as input.
+   - If `si` is greater than or equal to `ei`, it means there is only one element or none in the array, so no action is needed.
+   - Otherwise, it calculates the middle index `mid` of the array.
+   - Recursively divides the array into two halves: left part from `si` to `mid` and right part from `mid + 1` to `ei`.
+   - Calls the `merge` method to merge the sorted left and right halves.
+
+3. `merge` Method:
+   - Takes an integer array (`arr`), start index (`si`), middle index (`mid`), and end index (`ei`) as input.
+   - Creates a temporary integer array `temp` to hold the merged values.
+   - Initializes iterators `i` for the left part, `j` for the right part, and `k` for the `temp` array.
+   - Compares elements from the left and right parts and places the smaller element in the `temp` array until one of the parts is exhausted.
+   - Copies any remaining elements from the left and right parts to the `temp` array.
+   - Copies the elements from the `temp` array back to the original `arr` to merge the sorted parts.
+
+4. `main` Method:
+   - Initializes an integer array `arr` with unsorted values.
+   - Calls the `mergeSort` method to sort the `arr` array in ascending order.
+   - Prints the sorted array elements.
+
+In summary, this code demonstrates the Merge Sort algorithm, which efficiently sorts an array by dividing it into smaller parts, sorting those parts, and then merging them back together. The output of the program is the sorted array in ascending order. */
